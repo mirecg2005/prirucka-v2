72e25b5e370e190fe2d2ec1a9b742d79cb75d139
mirecg2005 <mirecg2005@gmail.com>
2026-07-12 22:12:03 +0200

    Oprava cesty k instalacii

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Príručka pre Vodičov",
   description: "Základná príručka pre vodičov flotily.",
-  manifest: "/manifest.json",
+  manifest: "/prirucka-v2/manifest.json",
   themeColor: "#ffffff",
   appleWebApp: {
     capable: true,
