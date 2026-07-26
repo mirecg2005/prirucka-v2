3af98c25d30706368228842b72a287556a84d6b2
mirecg2005 <mirecg2005@gmail.com>
2026-07-26 12:20:54 +0200

    Zmeny textov a potvrdzovacie tlacitko na telefonne cisla

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "build",
   basePath: "/prirucka-v2",
   images: {
     unoptimized: true,
